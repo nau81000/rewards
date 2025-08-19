095f1d4f2a0472795f95a90d213b6c069ef9402e
Nicolas Audheon <audheon.nicolas@gmail.com>
2025-08-19 11:01:55 +0200

    improve slack message and update architecture image

--- scripts/slack/redpanda_to_slack.py
+++ scripts/slack/redpanda_to_slack.py
@@ -7,42 +7,68 @@ from dotenv import load_dotenv
 from datetime import datetime, timezone
 from zoneinfo import ZoneInfo
 from babel.dates import format_datetime
-import os
+from os import getenv
 import json
+import psycopg2 as pg
 
-SLACK_TOKEN = os.getenv("SLACK_TOKEN")
-SLACK_CHANNEL = os.getenv("SLACK_CHANNEL")
-SLACK_USERNAME = os.getenv("SLACK_USERNAME")
+def main ():
+    """
+    """
+    # Load environment variables
+    load_dotenv()
+    SLACK_TOKEN = getenv("SLACK_TOKEN")
+    SLACK_CHANNEL = getenv("SLACK_CHANNEL")
+    SLACK_USERNAME = getenv("SLACK_USERNAME")
+    # Connect to DB
+    config = {
+           'host': getenv("POSTGRES_DB_HOST"),
+           'dbname': getenv("POSTGRES_DB_NAME"), 
+           'user': getenv("POSTGRES_ADMIN_USER"), 
+           'password': getenv("POSTGRES_ADMIN_PWD"),
+           'port': 5432
+    }
+    conn = pg.connect(**config)
+    cursor = conn.cursor()
+    # Connect to Slack
+    slack_client = WebClient(token=SLACK_TOKEN)
+    consumer = Consumer({
+        'bootstrap.servers': 'redpanda:9092',
+        'group.id': 'slack-poster',
+        'auto.offset.reset': 'earliest'
+    })
 
-load_dotenv()
-slack_client = WebClient(token=SLACK_TOKEN)
-consumer = Consumer({
-    'bootstrap.servers': 'redpanda:9092',
-    'group.id': 'slack-poster',
-    'auto.offset.reset': 'earliest'
-})
+    consumer.subscribe(['sport_data.public.activities'])
 
-consumer.subscribe(['sport_data.public.activities'])
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is None:
+            continue
+        if not msg.error():
+            payload = json.loads(msg.value().decode("utf-8"))['payload']
+            after_data = payload.get("after")
+            if after_data:
+                try:
+                    # Difference in microseconds
+                    delta_us = after_data['end_date'] - after_data['start_date']
+                    # Convert in hours
+                    delta_hours = delta_us / 1_000_000 / 3600
+                    # Get the activity date in french
+                    start_date = datetime.fromtimestamp(after_data['start_date'] / 1_000_000, tz=ZoneInfo("Europe/Paris"))
+                    formatted_date = format_datetime(start_date, "EEEE d MMMM yyyy", locale="fr_FR")
+                    # Get employee first and last names
+                    cursor.execute(f"SELECT first_name, last_name FROM employees WHERE id_employee={after_data['id_employee']}")
+                    result = cursor.fetchone()
+                    first_name = result[0]
+                    last_name = result[1]
+                    # Get sport name
+                    cursor.execute(f"SELECT sport FROM sports WHERE id_sport={after_data['id_sport']}")
+                    sport = cursor.fetchone()[0]
+                    # Building message
+                    text_msg= f"Bravo {first_name} {last_name}! Vous avez fait {int(delta_hours)} heures de {sport} le {formatted_date}"
+                    #print(f"Message envoyé: {text_msg}")
+                    slack_client.chat_postMessage(channel=SLACK_CHANNEL, text=text_msg, username=SLACK_USERNAME)
+                except SlackApiError as e:
+                    print(f"Erreur Slack: {e.response['error']}")
 
-while True:
-    msg = consumer.poll(1.0)
-    if msg is None:
-        continue
-    if not msg.error():
-        payload = json.loads(msg.value().decode("utf-8"))['payload']
-        after_data = payload.get("after")
-        if after_data:
-            try:
-                # Difference in microseconds
-                delta_us = after_data['end_date'] - after_data['start_date']
-                # Convert in hours
-                delta_hours = delta_us / 1_000_000 / 3600
-                # Get the activity date in french
-                start_date = datetime.fromtimestamp(after_data['start_date'] / 1_000_000, tz=ZoneInfo("Europe/Paris"))
-                formatted_date = format_datetime(start_date, "EEEE d MMMM yyyy", locale="fr_FR")
-                # Building message
-                text_msg= f"Bravo {after_data['id_employee']}! Vous avez fait {int(delta_hours)} heures de {after_data['id_sport']} le {formatted_date}"
-                #print(f"Message envoyé: {text_msg}")
-                slack_client.chat_postMessage(channel=SLACK_CHANNEL, text=text_msg, username=SLACK_USERNAME)
-            except SlackApiError as e:
-                print(f"Erreur Slack: {e.response['error']}")
+if __name__ == "__main__":
+    main()
